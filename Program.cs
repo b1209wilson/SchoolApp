623860bbdc8d598a83886335b10ee0115fbe6156
Mr. Wilson <bwilson@macon.k12.mo.us>
2021-01-26 15:23:04 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace SchoolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create First Student
+
+            Student FirstStudent = new Student();
+
+            // Get student info First Student
+
+            FirstStudent.StudentNum = AskForStudentNumber();
+
+            FirstStudent.StudentFirstName = AskForStudentName("First");
+
+            FirstStudent.StudentLastName = AskForStudentName("Last");
+
+            FirstStudent.Major = AskForMajor(FirstStudent.StudentFirstName);
+
+            FirstStudent.Score1 = AskForExamScore(1);
+
+            FirstStudent.Score2 = AskForExamScore(2);
+            
+            FirstStudent.Score3 = AskForExamScore(3);
+
+            Console.Clear();
+            Console.WriteLine("First Student");
+            Console.WriteLine(FirstStudent.ToString());
+
+            Console.WriteLine("Enter any key to stop program");
+            Console.ReadKey();
+
+
+
+        }
+
+        private static int AskForExamScore(int whichOne)
+        {
+
+            string inValue;
+            int aScore;
+
+            Console.WriteLine("Enter a value for score {0} :", whichOne);
+            inValue = Console.ReadLine();
+            aScore = int.Parse(inValue);
+            return aScore;
+
+        }
+
+        private static string AskForMajor(string name)
+        {
+            string inValue;
+            Console.WriteLine("Enter {0}\'s Major :", name);
+            inValue = Console.ReadLine();
+            return inValue;
+        }
+
+        private static string AskForStudentName(string whichOne)
+        {
+
+            string inValue;
+            Console.WriteLine("Enter Students {0} Name :", whichOne);
+            inValue = Console.ReadLine();
+            return inValue;
+
+
+
+        }
+
+        private static string AskForStudentNumber()
+        {
+            string inValue;
+            Console.WriteLine("Enter Students ID Number :");
+            inValue = Console.ReadLine();
+            return inValue;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolApp
+{
+    public class Student
+    {
+        // Instantiate Variables
+
+        private string studentNum, studentLastName, studentFirstName, major;
+        private int score1, score2, score3;
+
+        // Constructors
+
+        // default
+
+        public Student() 
+        { 
+        
+        }
+        
+        // Constructor - One Argument
+        public Student(string sID) 
+        {
+            studentNum = sID;
+
+        }
+
+        // Constructor - Three Arguments
+        public Student( string sID, string lastName, string firstName)
+        {
+
+            studentNum = sID;
+            studentLastName = lastName;
+            studentFirstName = firstName;
+
+
+        }
+
+        // Properties
+
+       
+        public string StudentNum { get => studentNum; set => studentNum = value; }
+        public string StudentLastName { get => studentLastName; set => studentLastName = value; }
+        public string StudentFirstName { get => studentFirstName; set => studentFirstName = value; }
+        public string Major { get => major; set => major = value; }
+        public int Score1 { get => score1; set => score1 = value; }
+        public int Score2 { get => score2; set => score2 = value; }
+        public int Score3 { get => score3; set => score3 = value; }
+
+        // Methods
+
+        public double CalculateAverage()
+        {
+
+            return (score1 + score2 + score3) / 3.0;
+
+        }
+
+        public override string ToString()
+        {
+            return "Name :" + studentFirstName + " " + studentLastName +
+                " \nMajor : " + major + " \nScore Average : " + CalculateAverage().ToString("*F2");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
